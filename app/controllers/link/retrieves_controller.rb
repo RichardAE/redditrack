6a91e4ef29b85728ed6660c7672bf8b7c0cf1a84
RichardAE <richard@raelwebsolutions.com>
2014-09-27 14:42:36 +0100

    Moved direct URL track into own block. Using existing service and adding via tracks controller instead. Just regex to go

--- app/controllers/link/retrieves_controller.rb
+++ app/controllers/link/retrieves_controller.rb
@@ -1,5 +1,7 @@
 class Link::RetrievesController < ApplicationController
   def create 
-    @track = Reddit::AddLinkViaUrl.new(current_user, params[:path]).process
+    client = Reddit::Shared.new.create_client(current_user)
+    @link = Reddit::SingleLink.new(client, "t3_2hliy2").retrieve
+    @track = current_user.tracks.new
   end
 end
